35fbf6252a0c0928f0bbe88b8110792ec88a41f3
patrykcegielka <patrykcegielka1994@gmail.com>
2022-12-10 09:57:48 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,9 @@
-const inputElement = document.getElementById("font-size-control");
-const spanElement = document.getElementById("text");
+let slider = document.getElementById("font-size-control");
+let text = document.getElementById("text");
 
-let changeFontSize = (event) => { 
-    let fontSize = event.target.value + "px";
-    spanElement.style.fontSize = fontSize;
-}
-
-inputElement.addEventListener("input", changeFontSize);
+slider.addEventListener("input", () => {
+  let size = slider.value;
+  text.style.fontSize = size + "px";
+});
 
 //Napisz skrypt, który reaguje na zmianę wartości input#font-size-control (zdarzenie input) i zmienia styl inline span#text aktualizując właściwość font-size. W rezultacie, podczas przeciągania paska przesuwania będzie zmieniał się rozmiar tekstu.
