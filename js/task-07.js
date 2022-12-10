f8dad0c6edaf5affa520defb2cdb7de0530a02ab
patrykcegielka <patrykcegielka1994@gmail.com>
2022-12-10 19:50:35 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 let slider = document.getElementById("font-size-control");
 let text = document.getElementById("text");
 
+slider.value = 16;
 slider.addEventListener("input", () => {
   let size = slider.value;
   text.style.fontSize = size + "px";
